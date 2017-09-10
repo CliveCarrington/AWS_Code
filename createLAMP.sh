5477236b72b5cd7ce3a0cc1f4d2fa3aaf74b3f3b
CliveCarrington <Clive.carrington@btinternet.com>
2017-09-10 20:50:45 +0100

    Created a basic install script
    Using http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-LAMP.html
    This is up to the point where the LAMP stand is setup and httpd is running
    It point to the next stage, which is setting up Security permissions, which is a manual task in the console

--- createLAMP.sh
+++ createLAMP.sh
@@ -3,4 +3,24 @@
 
 echo "This will become my main LAMP script"
 
+echo "Updating the system"
+sudo yum update -y
+echo "Now to install the Lamp stand"
+sudo yum install -y httpd24 php70 mysql56-server php70-mysqlnd
+echo "Installed the basic Lamp stand."
+sleep 5
+
+echo "Now to start the apache web server"
+
+sudo service httpd start
+
+sudo chkconfig httpd on
+
+echo "Now checking that the http is running"
+
+chkconfig --list httpd
+
+echo "Now follow the guidelines on security in the security.txt file"
+
+
 
